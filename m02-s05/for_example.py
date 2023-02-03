de6378f080d1603f04dbe707ad19a7b656277362
Ariel Calzada <ariel.calzada@gmail.com>
2023-02-02 21:08:45 -0500

    Add for example

---
+++ m02-s05/for_example.py
@@ -0,0 +1,16 @@
+# def main():
+#     word = "UCAMP"
+#
+#     for letter in word:
+#         print(letter)
+#
+#     print("Hello world")
+
+
+def main():
+    for n in range(10):
+        print(n)
+
+
+if __name__ == "__main__":
+    main()
